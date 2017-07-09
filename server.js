14ccbccece8bd607f6ac9cebf10855d35733eaaa
ElisaLuo <luolixuan2003@gmail.com>
2017-07-09 19:59:58 +0000

    deleted stormpath dependency because it is shutting down

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ var passport = require('passport');
 var session = require('express-session');
 var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectID;
-var sotrmpath = require('express-stormpath');
 var bodyParser = require('body-parser');
 var app = express();
 var mongoUrl = 'mongodb://elisal:pdnlxx021@ds151662.mlab.com:51662/build-a-voting-app';
@@ -22,11 +21,26 @@ app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use('/common', express.static(process.cwd() + '/app/common'));
 
-app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
-}));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', stormpath.getUser, function(req, res){
+	if(req.user){
+		res.render('pages/index', {user: req.user.email});
+	}
+	else{
+		res.render('pages/index', {user: null});
+	}
+});
+
+app.get('/singlePoll', stormpath.getUser, function(req, res){
+	if(req.user){
+		res.render('pages/singlePoll', {user: req.user.email});
+	}
+	else{
+		res.render('pages/singlePoll', {user: null});
+	}
+});
 
 app.use(passport.initialize());
 app.use(passport.session());
