90a82203260c0d676ff103e9d3c6dea4f7cb8f72
ElisaLuo <luolixuan2003@gmail.com>
2017-07-13 03:31:51 +0000

    started index.ejs page, setted up environment

--- server.js
+++ server.js
@@ -1,48 +1,20 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const session = require('client-sessions');
-const config = require('./config');
-const home = require('./routes/home');
-const polls = require('./routes/polls');
-const myPolls = require('./routes/mypolls');
-const createPoll = require('./routes/createpoll');
-const signUp = require('./routes/signup');
-const authCallack = require('./routes/callback');
-const login = require('./routes/login');
-const logout = require('./routes/logout');
-const app = express();
+var express = require('express');
+var fs = require('fs');
+var app = express();
+var mongodb = require('mongodb').MongoClient;
+var dbUrl = 'mongodb://elisal:pdnlxx021@ds151662.mlab.com:51662/build-a-voting-app';
+const index = require('./routes/index');
+var bodyParser = require('body-parser');
 
-process.env.NODE_ENV = 'production';
-
-mongoose.connect(config.database);
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error'));
-db.once('open', function () {
-  console.log('Connection successful');
-})
-
+app.set('port', process.env.PORT || process.env.IP );
 app.set('view engine', 'ejs');
-
-app.use(express.static('public'));
-app.use(session({
-  cookieName: 'session',
-  secret: 'bfksfysa7e32kdhayu292sz',
-  duration: 30 * 60 * 1000,
-}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', home);
-app.use('/polls', polls);
-app.use('/createpoll', createPoll);
-app.use('/mypolls',myPolls);
-app.use('/signup', signUp);
-app.use('/callback',authCallack);
-app.use('/login', login);
-app.use('/logout', logout);
+app.use("/", index);
 
-
-app.listen(config.port);
-module.exports = app; 
+var port = process.env.PORT || 3000;
+app.listen(port,  function () {
+	console.log('Node.js listening on port ' + port + '...');
+});
